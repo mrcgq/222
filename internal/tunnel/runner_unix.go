bbbfeec337a08353135f013ab41d2c0a05ea445a
mrcgq <pennymrs413@gmail.com>
2026-02-20 15:41:32 +0800

    Create runner_unix.go

---
+++ internal/tunnel/runner_unix.go
@@ -0,0 +1,38 @@
+//go:build linux || darwin || freebsd
+
+package tunnel
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+// configurePlatformProcess 配置平台特定的进程属性 (Unix)
+func configurePlatformProcess(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true,
+	}
+}
+
+// terminateProcess 优雅终止进程 (Unix)
+func terminateProcess(process *os.Process) {
+	if process == nil {
+		return
+	}
+	pid := process.Pid
+	// 向进程组发送 SIGTERM
+	_ = syscall.Kill(-pid, syscall.SIGTERM)
+}
+
+// killProcess 强制终止进程 (Unix)
+func killProcess(process *os.Process) {
+	if process == nil {
+		return
+	}
+	pid := process.Pid
+	// 向进程组发送 SIGKILL
+	_ = syscall.Kill(-pid, syscall.SIGKILL)
+	// 作为后备，也直接 Kill 进程
+	_ = process.Kill()
+}
